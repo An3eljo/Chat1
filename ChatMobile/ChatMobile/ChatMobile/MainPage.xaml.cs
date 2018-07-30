cbd99a074d6277cc9ccfc85e8c2d0e06a1d9c12d
andrijoos <andri.joos@edu.tbz.ch>
2018-07-30 18:03:59 +0200

    change mainpage

--- ChatMobile/ChatMobile/ChatMobile/MainPage.xaml.cs
+++ ChatMobile/ChatMobile/ChatMobile/MainPage.xaml.cs
@@ -3,15 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
 using Xamarin.Forms;
 
 namespace ChatMobile
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+	    private string _message1;
+
+	    public string Message1
+	    {
+	        get { return _message1; }
+	        set
+	        {
+	            _message1 = value;
+	            OnPropertyChanged();
+	        }
+	    }
+
+	    public MainPage()
 		{
 			InitializeComponent();
-		}
+		    Test();
+        }
+
+	    private void Test()
+	    {
+	        var client = new MqttFactory().CreateMqttClient();
+
+	        var options = new MqttClientOptionsBuilder().WithClientId("testuser1").WithTcpServer("192.168.43.91", 1884)
+	            .Build();
+
+	        client.ConnectAsync(options).Wait();
+            //Task.Run(async () => await client.ConnectAsync(options));
+
+	        client.SubscribeAsync("TestTopic").Wait();
+
+	        client.ApplicationMessageReceived += (sender, e) =>
+	        {
+	            Message1 = e.ClientId + "has written: " + Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+	        };
+
+            var message = new MqttApplicationMessageBuilder().WithTopic("TestTopic").WithPayload("testmessageClient").Build();
+
+	        client.PublishAsync(message).Wait();
+
+            //Task.Run(async () => await client.PublishAsync(message));
+            //Task.Run(
+            //    async () => await client.SubscribeAsync(new TopicFilterBuilder().WithTopic("testmessage1").Build()));
+
+	        
+
+	        //client.DisconnectAsync().Wait();
+	        //Task.Run(async () => await client.DisconnectAsync());
+	    }
 	}
 }
