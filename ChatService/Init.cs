97ab5acf5b353a1205ca150f95775e7caaddae8c
andrijoos <andri.joos@edu.tbz.ch>
2018-07-30 17:41:50 +0200

    tt

---
+++ ChatService/Init.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using ChatServer;
+
+namespace ChatService
+{
+    public class Init
+    {
+        public static void Initialize()
+        {
+            CheckAboutOtherInstance();
+        }
+
+        static void CheckAboutOtherInstance()
+        {
+            if (System.Diagnostics.Process
+                    .GetProcessesByName(
+                        System.IO.Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly()
+                            .Location)).Count() > 1)
+            {
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+                return;
+            }
+
+            var uiThread = new Thread(() =>
+            {
+                var ui = new Application();
+                ui.Run(new MainWindow());
+            });
+
+            uiThread.SetApartmentState(ApartmentState.STA);
+            uiThread.Start();
+        }
+    }
+}
--- ChatService/Program.cs
+++ ChatService/Program.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace ChatService
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Init.Initialize();
+            Console.ReadKey();
         }
     }
 }
